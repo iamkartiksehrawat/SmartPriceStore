f67e0ec6c1dcef1364b54cc093ef4642176d9f81
Kartik <sehrawatkar@gmail.com>
2024-03-06 14:42:39 +0530

    Front Page Integration

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const index = require("./routes/index.js");
 const app = express();
 
+app.use(express.static("public"));
+
 app.use("/", index);
 
 app.listen(3000);
