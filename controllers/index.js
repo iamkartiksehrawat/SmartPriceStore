212e8a2daf437a6b4c171c1faa2e5461ed956322
Kartik <sehrawatkar@gmail.com>
2024-03-09 12:30:14 +0530

    Minor changes

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,18 @@
-exports.getpage = (req, res) => {
+const Cart = require("../models/cart");
+
+exports.getpage = async (req, res) => {
   let auth = req.session ? req.session.isauth : false;
+  let len = 0;
+  if (auth) {
+    let usrid = req.session.user._id;
+    let cart = await Cart.findOne({ userID: usrid });
+    if (cart) {
+      len = cart.products.length;
+    }
+  }
   res.render("index.ejs", {
     isauth: auth,
+    cartlen: len,
   });
 };
 
